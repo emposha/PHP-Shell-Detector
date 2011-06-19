ffe0666f561195ca1801990ad2c5c33feff8552a
unknown <admin@emposha.com>
2011-06-19 19:01:14 +0300

    source line show added

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function init() {
-  $('dt').click(function() {
-    var text = $(this).children('.plus');
+  $("dt").click(function() {
+    var text = $(this).children(".plus");
     if (text.length) {
-      $(this).next('dd').slideToggle();
-      if (text.text() == '+') {
-        text.text('-');
+      $(this).next("dd").slideToggle();
+      if (text.text() == "+") {
+        text.text("-");
       }
       else {
-        text.text('+');
+        text.text("+");
       }
     }
+  });
+  $(".showline").click(function() {
+    var id = "li"+$(this).attr("id");
+    $( "#"+ id).dialog({height: 440,modal: true, width: 600, title: "Source code"});
+    return false;
   });
 }
 $(document).ready(init);
--- shelldetect.php
+++ shelldetect.php
@@ -52,7 +52,9 @@ class shellDetector {
           for ($line = 0; $line < count($content); $line++) {
             if (preg_match_all('%(passthru|shell_exec|exec|base64_decode|eval|system|proc_open|popen|curl_exec|curl_multi_exec|parse_ini_file|show_source)%', $content[$line], $matches)) {
               $flag = true;
-              $output .= $this->_implode($matches).' ('.$this->t('line:').$line.'); ';
+              $lineid = md5($line.$file);
+              $output .= $this->_implode($matches).' (<a href="#" class="showline" id="ne_'.$lineid.'">'.$this->t('line:').$line.'</a>);';
+              $output .= '<div class="hidden source" id="line_'.$lineid.'"><code>'.$content[$line].'</code></div>';
             }
           }
           if ($flag) {
@@ -90,9 +92,9 @@ class shellDetector {
   }
   
   private function header() {
-    $style = '<style type="text/css" media="all">body {background-color: #ccc;font: 13px tahoma, arial; color: #151515; direction: ltr;}h1{text-align:center;font-size:24px;}dl{margin:0px; padding:0px;}#content {width: 1024px;margin:0px auto;padding:35px 40px;border:1px solid #e8e8e8;background:#fff;overflow:hidden;-webkit-border-radius:7px;-moz-border-radius:7px;border-radius:7px;}dl dt{cursor: pointer;background:#5f9be3;color:#fff;float:left;font-weight:700;margin-right:10px;width:99%;position:relative;padding:5px}dl dt .plus{position:absolute;right:4px}dl dd{margin:2px 0;padding:5px 0}dl dd dl{margin-top:24px;margin-left:60px}dl dd dl dt{background:#4FCBA3!important;width:180px!important} .error{background-color: #FFEBE8;border: 1px solid #DD3C10;padding:4px 10px;margin: 5px 0px} .info{background-color:#fff9d7;border: 1px solid #e2c822;padding:4px 10px;margin: 5px 0px}.clearer{clear:both;height:0px;font-size:0px;}</style>';
-    $script = '<script type="text/javascript">function init(){$(\'dt\').click(function(){var text=$(this).children(\'.plus\');if(text.length){$(this).next(\'dd\').slideToggle();if(text.text()==\'+\'){text.text(\'-\')}else{text.text(\'+\')}}})}$(document).ready(init);</script>';
-    $this->output('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>PHP Shell Detector</title>'.$style.'<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.min.js" type="text/javascript" charset="utf-8"></script>'.$script.'</head><body><h1>'.$this->title.'</h1><div id="content">', null, false);
+    $style = '<style type="text/css" media="all">body {background-color: #ccc;font: 13px tahoma, arial; color: #151515; direction: ltr;}h1{text-align:center;font-size:24px;}dl{margin:0px; padding:0px;}#content {width: 1024px;margin:0px auto;padding:35px 40px;border:1px solid #e8e8e8;background:#fff;overflow:hidden;-webkit-border-radius:7px;-moz-border-radius:7px;border-radius:7px;}dl dt{cursor: pointer;background:#5f9be3;color:#fff;float:left;font-weight:700;margin-right:10px;width:99%;position:relative;padding:5px}dl dt .plus{position:absolute;right:4px}dl dd{margin:2px 0;padding:5px 0}dl dd dl{margin-top:24px;margin-left:60px}dl dd dl dt{background:#4FCBA3!important;width:180px!important} .error{background-color: #FFEBE8;border: 1px solid #DD3C10;padding:4px 10px;margin: 5px 0px} .info{background-color:#fff9d7;border: 1px solid #e2c822;padding:4px 10px;margin: 5px 0px}.clearer{clear:both;height:0px;font-size:0px;}.hidden {display:none;}</style>';
+    $script = 'function init(){$("dt").click(function(){var text=$(this).children(".plus");if(text.length){$(this).next("dd").slideToggle();if(text.text()=="+"){text.text("-")}else{text.text("+")}}});$(".showline").click(function(){var id="li"+$(this).attr("id");$("#"+id).dialog({height:440,modal:true,width:600,title:"Source code"});return false;})}$(document).ready(init);';
+    $this->output('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>PHP Shell Detector</title>'.$style.'<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/themes/base/jquery-ui.css" type="text/css" media="all" /><script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.min.js" type="text/javascript" charset="utf-8"></script><script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js" type="text/javascript" charset="utf-8"></script><script type="text/javascript">'.$script.'</script></head><body><h1>'.$this->title.'</h1><div id="content">', null, false);
   }
   
   private function output($content, $class = 'info', $html = true) {
