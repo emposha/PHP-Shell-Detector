d903cd224219fdeb2731dec06f8d5cc191109000
warmwinter <github@miss77.net>
2012-08-03 01:00:41 +0800

    new Chinese language file

--- lang/chinese.php
+++ lang/chinese.php
@@ -1,8 +1,6 @@
 <?php
   $local = array();
   $local['Please login to continue.'] = '请登录后继续。';
-  $local['Please note <strong>openssl</strong> library not found, suspicious files will be included in html without encryption.'] = '请注意：没有发现 <strong>openssl</strong> 扩展库。可疑文件将只包含未被加密的文件。';
-  $local['Please note, shells signature database not found, suspicious files will be scan only by behavior.'] = '请注意：未发现 shell 签名数据库。将只通过行为扫描可疑文件。';
   $local['Shells signature database updated succesfully!'] = 'Shell 签名数据库更新成功！';
   $local['Cant save shells signature database please check permissions'] = '不能保存 shell 签名数据库，请检查权限。';
   $local['Your shells signatures database already updated!'] = '您的 shell 签名数据库已经更新！';
@@ -10,6 +8,9 @@
   $local['Cant connect to server! Version check failed!'] = '无法连接到服务器！版本检查失败！';
   $local['New version of application found. Please update!'] = '发现新版本的程序。请更新！';
   $local['Cant connect to server! Application version check failed!'] = '无法连接到服务器！程序版本检查失败！';
+  $local['You code submited. Thank you.'] = '您的代码已提交。非常感谢。';
+  $local['Cant find selected file.'] = '未找到选择的文件。';
+  $local['No file specified.'] = '未指定文件。';
   $local['Starting file scanner, please be patient file scanning can take some time.'] = '开始扫描文件，请耐心等待，这需要花一些时间。';
   $local['Number of known shells in database is: '] = '数据库中已知的 shell 数量：';
   $local['Files found:'] = '已发现的文件：';
@@ -22,11 +23,6 @@
   $local['please note it`s a malicious file not a shell'] = '请注意：它是一个恶意文件，不是 shell。';
   $local['Fingerprint:'] = '指纹：';
   $local['Positive, it`s a '] = '实际上，它是一个 ';
-  $local['Owner:'] = '所有者：';
-  $local['Permission:'] = '权限：';
-  $local['Last accessed:'] = '最后访问时间：';
-  $local['Last modified:'] = '最后修改时间：';
-  $local['Filesize:'] = '文件大小：';
   $local['suspicious functions used:'] = '使用的可疑函数：';
   $local['line:'] = '行号：';
   $local['<strong>Status</strong>: @count suspicious files found and @shells shells found'] = '<strong>状态</strong>：已发现 @count 个可疑文件以及 @shells 个 shell';
@@ -37,9 +33,13 @@
   $local['Your email'] = '您的 Email';
   $local['(in case you want to be notified):'] = '(如果你想获得通知)：';
   $local['Suspicious behavior found in:'] = '发现可疑行为于：';
+  $local['Owner:'] = '所有者：';
+  $local['Permission:'] = '权限：';
+  $local['Last accessed:'] = '最后访问时间：';
+  $local['Last modified:'] = '最后修改时间：';
   $local['MD5 hash:'] = 'MD5 哈希值：';
+  $local['Filesize:'] = '文件大小：';
   $local['Please wait'] = '请稍候...';
   $local['<strong>Error: </strong>'] = '<strong>错误：</strong>';
   $local[' line: '] = '行号：';
-  //$local[''] = '';
 ?>
